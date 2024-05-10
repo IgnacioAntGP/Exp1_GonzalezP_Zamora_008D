9ba0803ea5b8c245760e2ed2d46e30cf0cb321e8
IgnacioAntGP <ignaa.gonzalezp@duocuc.cl>
2024-05-10 01:43:47 -0400

    Registro distribuidor completo

--- js/validate.js
+++ js/validate.js
@@ -8,14 +8,41 @@ $(function(){
                 required: true,
                 minlength: 3
             },
-            pass1: "required",
-            pass2:{
+            appat:"required",
+            apmat: "required",
+            correo:{
                 required: true,
-                equalTo: "#pass1"
+                email: true
+            },
+            tel:{
+                required: true,
+                digits: true,
+                maxlength: 10
+            },
+            vehiculo:"required",
+            producto:{
+                required: true,
+                minlength: 2
             }
         },
         messages:{
-            nombre: "Debe ingresar un mínimo de 3 caracteres"
+            nombre:{
+                required: "Debe ingresar un mínimo de 3 caracteres",
+                minlength: "Debe tener un mínimo de 3 caracteres"
+            },
+            correo:{
+                required: "Debe llenar este campo",
+                email: "Debe ingresar un correo válido"
+            },
+            tel:{
+                required: "Debe llenar este campo",
+                maxlength: "Debe ingresar un teléfono válido"
+            },
+            vehiculo: "Debe ingresar una opcion",
+            producto:{
+                required: "Debe llenar este campo",
+                minlength: "Debe escoger un mínimo de 2 opciones"
+            }
         }
     });
 });
